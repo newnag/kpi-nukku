71ff0cd15a9a5c2e92bcf4e6fcfd5bc4c5d0aa40
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-07-29 15:09:36 +0700

    [ADD] Checklist_item, Model and Migrate file

---
+++ app/Models/Checklist_item.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Checklist_item extends Model
+{
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'required_items',
+        'score',
+        'description',
+        'indicator_id',
+    ];
+
+    public function indicator()
+    {
+        return $this->belongsTo(Indicator::class);
+    }
+}
---
+++ database/migrations/2025_07_29_070501_create_checklist_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checklist_items', function (Blueprint $table) {
+            $table->id();
+            $table->json('required_items')->nullable();
+            $table->float('score', 5, 2)->nullable();
+            $table->text('description')->nullable();
+
+            $table->foreignId('indicator_id')
+                ->constrained('indicators')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checklist_items');
+    }
+};
