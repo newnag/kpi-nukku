babec338c4494c58854065a2c0b0323cbf932358
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-07-29 15:10:00 +0700

    [ADD] Assignment, Model and Migrate file

---
+++ app/Models/Assignment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'indicator_id',
+        'collector',
+    ];
+
+    public function indicator()
+    {
+        return $this->belongsTo(Indicator::class);
+    }
+
+    public function collector()
+    {
+        return $this->belongsTo(User::class, 'collector');
+    }
+}
