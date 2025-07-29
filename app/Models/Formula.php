69f48d9c45e07befb691ba2ec8a60e26e51a0a50
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-07-29 15:09:00 +0700

    [ADD] Variablr , Formula and Variable_formula .Model and Migrate file

---
+++ app/Models/Formula.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Formula extends Model
+{
+    protected $fillable = [
+        'condition',
+        'indicator_id',
+    ];
+
+    public function indicator()
+    {
+        return $this->belongsTo(Indicator::class);
+    }
+
+    public function variables()
+    {
+        return $this->belongsToMany(Variable::class, 'variable_formulas');
+    }
+}
---
+++ database/migrations/2025_07_29_064529_create_formulas_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('variables', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->string('variable_name');
+            $table->string('status');
+            $table->float('value', 5, 2)->nullable();
+            
+            $table->foreignId('indicator_id')
+            ->constrained('indicators')
+            ->cascadeOnDelete();
+            
+            $table->timestamps();
+        });
+
+        Schema::create('formulas', function (Blueprint $table) {
+            $table->id();
+            $table->text('condition');
+            $table->timestamps();
+
+            $table->foreignId('indicator_id')
+                ->constrained('indicators')
+                ->cascadeOnDelete();
+        });
+
+        Schema::create('variable_formulas', function (Blueprint $table) {
+            $table->foreignId('variable_id')
+                ->constrained('variables')
+                ->cascadeOnDelete();
+
+            $table->foreignId('formula_id')
+                ->constrained('formulas')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('variable_formulas');
+        Schema::dropIfExists('formulas');
+        Schema::dropIfExists('variables');
+    }
+};
