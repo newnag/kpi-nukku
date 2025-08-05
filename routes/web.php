c5dc1eb3b2a287df56c54c38b94c0585792c82b1
พิสุทธิ์ พาละพล <55123204+Pisutpa@users.noreply.github.com>
2025-08-05 16:03:22 +0700

    Add app layout view and update web routes for department management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('/layouts/app');
+// });
+
+Route::prefix('departments')->name('departments.')->group(function () {
+    Route::get('/', [DepartmentController::class, 'index'])->name('index');
+    Route::post('/store', [DepartmentController::class, 'store'])->name('store');
+    // Add other routes for departments here
 });
