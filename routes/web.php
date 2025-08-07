492606146d9303647cdf173fafe2dbfa996da1b3
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-08-07 10:06:27 +0700

    [add] Route indicator dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/indicator/dashboard', [\App\Http\Controllers\IndicatorController::class, 'index'])->name('indicator.dashboard');
