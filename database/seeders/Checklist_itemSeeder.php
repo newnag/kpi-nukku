ba0e495a958a5e5cf474446ef23e65124447ff9a
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-08-29 09:45:43 +0700

    [update] updating seeder in new database

--- database/seeders/Checklist_itemSeeder.php
+++ database/seeders/Checklist_itemSeeder.php
@@ -13,319 +13,286 @@ class Checklist_itemSeeder extends Seeder
     public function run(): void
     {
         DB::table('checklist_items')->insert([
-            // //ตัวบ่งชี้ที่ 1
+            // ตัวบ่งชี้ที่ 1
             [
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 10,
-                'description' => 'รายละเอียดของรายการตรวจสอบตัวอย่าง 1',
+                'sequence' => 1,
                 'indicator_id' => 1,
-
             ],
-            //ตัวบ่งชี้ที่ 4
+            // ตัวบ่งชี้ที่ 4
             [
                 'indicator_id' => 4,
                 'required_items' => json_encode(['1', '2']),
                 'score' => 5,
-                'description' => 'รายละเอียดของรายการตรวจสอบตัวอย่าง 2',
-
+                'sequence' => 1,
             ],
-
-            //ตัวบ่งชี้ที่ 8
+            // ตัวบ่งชี้ที่ 8
             [
                 'indicator_id' => 8,
                 'required_items' => json_encode(['1', '2', '3', '4', '5', '6', '7']),
                 'score' => 15,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 9
+            // ตัวบ่งชี้ที่ 9
             [
                 'indicator_id' => 9,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 10
+            // ตัวบ่งชี้ที่ 10
             [
                 'indicator_id' => 10,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 50,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 11
+            // ตัวบ่งชี้ที่ 11
             [
                 'indicator_id' => 11,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 20,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 12
+            // ตัวบ่งชี้ที่ 12
             [
                 'indicator_id' => 12,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 5,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 13
+            // ตัวบ่งชี้ที่ 13
             [
                 'indicator_id' => 13,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 5,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 14
+            // ตัวบ่งชี้ที่ 14
             [
                 'indicator_id' => 14,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 5,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 15
+            // ตัวบ่งชี้ที่ 15
             [
                 'indicator_id' => 15,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 5,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 16
+            // ตัวบ่งชี้ที่ 16
             [
                 'indicator_id' => 16,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 17
+            // ตัวบ่งชี้ที่ 17
             [
                 'indicator_id' => 17,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 15,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 18
+            // ตัวบ่งชี้ที่ 18
             [
                 'indicator_id' => 18,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 19
+            // ตัวบ่งชี้ที่ 19
             [
                 'indicator_id' => 19,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 10,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 20
+            // ตัวบ่งชี้ที่ 20
             [
                 'indicator_id' => 20,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 10,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 21
+            // ตัวบ่งชี้ที่ 21
             [
                 'indicator_id' => 21,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 10,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 22
+            // ตัวบ่งชี้ที่ 22
             [
                 'indicator_id' => 22,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 10,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 23
+            // ตัวบ่งชี้ที่ 23
             [
                 'indicator_id' => 23,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 15,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 24
+            // ตัวบ่งชี้ที่ 24
             [
                 'indicator_id' => 24,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 15,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 25
+            // ตัวบ่งชี้ที่ 25
             [
                 'indicator_id' => 25,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 26
+            // ตัวบ่งชี้ที่ 26
             [
                 'indicator_id' => 26,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 25,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 27
+            // ตัวบ่งชี้ที่ 27
             [
                 'indicator_id' => 27,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 25,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 28
+            // ตัวบ่งชี้ที่ 28
             [
                 'indicator_id' => 28,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 29
+            // ตัวบ่งชี้ที่ 29
             [
                 'indicator_id' => 29,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 30
+            // ตัวบ่งชี้ที่ 30
             [
                 'indicator_id' => 30,
                 'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 31
+            // ตัวบ่งชี้ที่ 31
             [
                 'indicator_id' => 31,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 32
+            // ตัวบ่งชี้ที่ 32
             [
                 'indicator_id' => 32,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 15,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 33
+            // ตัวบ่งชี้ที่ 33
             [
                 'indicator_id' => 33,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 15,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 34
+            // ตัวบ่งชี้ที่ 34
             [
                 'indicator_id' => 34,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 10,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 35
+            // ตัวบ่งชี้ที่ 35
             [
                 'indicator_id' => 35,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 20,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 36
+            // ตัวบ่งชี้ที่ 36
             [
                 'indicator_id' => 36,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 20,
-                'description' => '',
+                'sequence' => 1,
             ],
-            //ผลลัพธ์
-            //ตัวบ่งชี้ที่ 1
+            // ผลลัพธ์
+            // ตัวบ่งชี้ที่ 1
             [
                 'indicator_id' => 37,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 20,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 2
+            // ตัวบ่งชี้ที่ 2
             [
                 'indicator_id' => 38,
-                'required_items' => json_encode(['1', '2',]),
+                'required_items' => json_encode(['1', '2']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 11
+            // ตัวบ่งชี้ที่ 11
             [
                 'indicator_id' => 47,
                 'required_items' => json_encode(['1', '2']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 12
+            // ตัวบ่งชี้ที่ 12
             [
                 'indicator_id' => 48,
-                'required_items' => json_encode(['1', '2', '3', '4','5']),
+                'required_items' => json_encode(['1', '2', '3', '4', '5']),
                 'score' => 5,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 14
+            // ตัวบ่งชี้ที่ 14
             [
                 'indicator_id' => 50,
                 'required_items' => json_encode(['1', '2', '3', '4']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 15
+            // ตัวบ่งชี้ที่ 15
             [
                 'indicator_id' => 51,
                 'required_items' => json_encode(['1', '2']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 16
+            // ตัวบ่งชี้ที่ 16
             [
                 'indicator_id' => 52,
-                'required_items' => json_encode(['1', '2' ]),
+                'required_items' => json_encode(['1', '2']),
                 'score' => 20,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 18
+            // ตัวบ่งชี้ที่ 18
             [
                 'indicator_id' => 54,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 25,
-                'description' => '',
-
+                'sequence' => 1,
             ],
-            //ตัวบ่งชี้ที่ 20
+            // ตัวบ่งชี้ที่ 20
             [
                 'indicator_id' => 56,
                 'required_items' => json_encode(['1', '2', '3']),
                 'score' => 10,
-                'description' => '',
-
+                'sequence' => 1,
             ],
         ]);
     }
--- database/seeders/VariableSeeder.php
+++ database/seeders/VariableSeeder.php
@@ -17,7 +17,6 @@ class VariableSeeder extends Seeder
             [
                 //id-1
                 'indicator_id' => 2,
-                'label' => 'ร้อยละของอาจารย์ประจำที่มีคุณวุฒิปริญญาเอก',
                 'variable_name' => 'precent_doctor',
                 'type' => 'input,',
                 'value' => null,
@@ -27,7 +26,6 @@ class VariableSeeder extends Seeder
             [
                 //id-2
                 'indicator_id' => 2,
-                'label' => 'ร้อยละ 40',
                 'variable_name' => 'precent_40',
                 'type' => 'static',
                 'value' => 40.00,
@@ -37,7 +35,6 @@ class VariableSeeder extends Seeder
             [
                 //id-3
                 'indicator_id' => 2,
-                'label' => 'คะแนน',
                 'variable_name' => 'socre',
                 'type' => 'static',
                 'value' => 10.00,
@@ -47,7 +44,6 @@ class VariableSeeder extends Seeder
             [
                 //id-4
                 'indicator_id' => 2,
-                'label' => 'คะแนนที่ได้',
                 'variable_name' => 'score',
                 'type' => 'output',
                 'value' => null,
@@ -57,7 +53,6 @@ class VariableSeeder extends Seeder
             [
                 // id-5
                 'indicator_id' => 3,
-                'label' => 'จำนวนอาจารย์พยาบาลประจำที่มีคุณสมบัติตามเกณฑ์',
                 'variable_name' => 'qualified_nurse',
                 'type' => 'input',
                 'value' => null,
@@ -67,7 +62,6 @@ class VariableSeeder extends Seeder
             [
                 // id-6
                 'indicator_id' => 3,
-                'label' => 'จำนวนอาจารย์พยาบาลประจำทั้งหมดของสถาบันรวมทุกคุณวุฒิการศึกษา',
                 'variable_name' => 'total_nurse',
                 'type' => 'input',
                 'value' => null,
@@ -77,7 +71,6 @@ class VariableSeeder extends Seeder
             [
                 // id-7
                 'indicator_id' => 3,
-                'label' => 'คะแนน',
                 'variable_name' => 'socre',
                 'type' => 'static',
                 'value' => 100.00,
@@ -87,7 +80,6 @@ class VariableSeeder extends Seeder
             [
                 // id-8
                 'indicator_id' => 3,
-                'label' => 'คะแนนที่ได้',
                 'variable_name' => 'socre',
                 'type' => 'output',
                 'value' => null,
@@ -99,7 +91,6 @@ class VariableSeeder extends Seeder
             [
                 // id-9
                 'indicator_id' => 5,
-                'label' => 'ร้อยละของอาจารย์ประจำหลักสูตร',
                 'variable_name' => 'precent_teacher',
                 'type' => 'input',
                 'value' => null,
@@ -109,7 +100,6 @@ class VariableSeeder extends Seeder
             [
                 // id-10
                 'indicator_id' => 5,
-                'label' => 'ตัวแปร',
                 'variable_name' => 'precent_100',
                 'type' => 'static',
                 'value' => 100.00,
@@ -119,7 +109,6 @@ class VariableSeeder extends Seeder
             [
                 // id-11
                 'indicator_id' => 5,
-                'label' => 'ตัวแปร',
                 'variable_name' => 'score',
                 'type' => 'static',
                 'value' => 15.00,
@@ -129,7 +118,6 @@ class VariableSeeder extends Seeder
             [
                 // id-12
                 'indicator_id' => 5,
-                'label' => 'คะแนนที่ได้',
                 'variable_name' => 'score',
                 'type' => 'output',
                 'value' => null,
@@ -140,7 +128,6 @@ class VariableSeeder extends Seeder
             [
                 // id-13
                 'indicator_id' => 6,
-                'label' => 'อัตราส่วนของอาจารย์ประจำต่อจำนวนนักศึกษา',
                 'variable_name' => 'input',
                 'type' => 'input',
                 'value' => null,
@@ -151,7 +138,6 @@ class VariableSeeder extends Seeder
             [
                 // id-14
                 'indicator_id' => 7,
-                'label' => 'ร้อยละของรายวิชาที่มีอาจารย์พยาบาลประจำต่อนักศึกษาไม่เกิน 1:8',
                 'variable_name' => 'input',
                 'type' => 'input',
                 'value' => null,
@@ -162,7 +148,6 @@ class VariableSeeder extends Seeder
             [
                 // id-15
                 'indicator_id' => 37,
-                'label' => 'คะแนนเฉลี่ยของการประเมินคณะผู้บริหาร',
                 'variable_name' => 'average_score',
                 'type' => 'input',
                 'value' => null,
@@ -173,7 +158,6 @@ class VariableSeeder extends Seeder
             [
                 // id-16
                 'indicator_id' => 39,
-                'label' => 'ร้อยละของการดำเนินการที่บรรลุเป้าหมายตามแผนกลยุทธ์/ยุทธศาสตร์',
                 'variable_name' => 'precent_100',
                 'type' => 'input',
                 'value' => null,
@@ -183,7 +167,6 @@ class VariableSeeder extends Seeder
             [
                 // id-17
                 'indicator_id' => 40,
-                'label' => 'ร้อยละของการใช้จ่ายงบประมาณตามแผนจัดสรรงบประมาณ',
                 'variable_name' => 'precent_budget',
                 'type' => 'input',
                 'value' => null,
@@ -193,7 +176,6 @@ class VariableSeeder extends Seeder
             [
                 // id-18
                 'indicator_id' => 41,
-                'label' => 'อัตราการคงอยู่ของอาจารย์',
                 'variable_name' => 'retention_rate',
                 'type' => 'input',
                 'value' => null,
@@ -202,7 +184,6 @@ class VariableSeeder extends Seeder
             [
                 // id-19
                 'indicator_id' => 42,
-                'label' => 'อัตราการคงอยู่ของบุคลากรสายสนับสนุน',
                 'variable_name' => 'retention_support_staff',
                 'type' => 'input',
                 'value' => null,
@@ -213,7 +194,6 @@ class VariableSeeder extends Seeder
             [
                 // id-20
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน  ระดับที่ 1',
                 'variable_name' => 'quality_management_level_1',
                 'type' => 'input',
                 'value' => null,
@@ -222,7 +202,6 @@ class VariableSeeder extends Seeder
             [
                 // id-21
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 1 คิดเป็นร้อยละ',
                 'variable_name' => 'quality_management_level_1_percent',
                 'type' => 'input',
                 'value' => null,
@@ -231,7 +210,6 @@ class VariableSeeder extends Seeder
             [
                 // id-22
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 2',
                 'variable_name' => 'quality_management_level_2',
                 'type' => 'input',
                 'value' => null,
@@ -240,7 +218,6 @@ class VariableSeeder extends Seeder
             [
                 // id-23
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 2 คิดเป็นร้อยละ',
                 'variable_name' => 'quality_management_level_2_percent',
                 'type' => 'input',
                 'value' => null,
@@ -249,7 +226,6 @@ class VariableSeeder extends Seeder
             [
                 // id-24
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 3',
                 'variable_name' => 'quality_management_level_3',
                 'type' => 'input',
                 'value' => null,
@@ -258,7 +234,6 @@ class VariableSeeder extends Seeder
             [
                 // id-25
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 3 คิดเป็นร้อยละ',
                 'variable_name' => 'quality_management_level_3_percent',
                 'type' => 'input',
                 'value' => null,
@@ -267,7 +242,6 @@ class VariableSeeder extends Seeder
             [
                 // id-26
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 4',
                 'variable_name' => 'quality_management_level_4',
                 'type' => 'input',
                 'value' => null,
@@ -276,7 +250,6 @@ class VariableSeeder extends Seeder
             [
                 // id-27
                 'indicator_id' => 43,
-                'label' => 'คุณภาพการจัดการเรียนการสอน ระดับที่ 4 คิดเป็นร้อยละ',
                 'variable_name' => 'quality_management_level_4_percent',
                 'type' => 'input',
                 'value' => null,
@@ -285,7 +258,6 @@ class VariableSeeder extends Seeder
             [
                 //id-28
                 'indicator_id' => 44,
-                'label' => 'ร้อยละนักศึกษาชั้นปีสุดท้ายที่มีผลลัพธ์การเรียนรู้ครบตามที่กำหนด',
                 'variable_name' => 'precent_outcome',
                 'type' => 'input',
                 'value' => null,
@@ -294,7 +266,6 @@ class VariableSeeder extends Seeder
             [
                 //id-29
                 'indicator_id' => 45,
-                'label' => 'ร้อยละของผู้สอบผ่านในครั้งแรก',
                 'variable_name' => 'precent_pass_first_attempt',
                 'type' => 'input',
                 'value' => null,
@@ -303,7 +274,6 @@ class VariableSeeder extends Seeder
             [
                 //id-30
                 'indicator_id' => 46,
-                'label' => 'ร้อยละของผู้สอบผ่านในปีแรก',
                 'variable_name' => 'precent_pass_first_year',
                 'type' => 'input',
                 'value' => null,
@@ -312,7 +282,6 @@ class VariableSeeder extends Seeder
             [
                 //id-31
                 'indicator_id' => 49,
-                'label' => 'ร้อยละของผลงานทางวิชาการ',
                 'variable_name' => 'precent_academic_work',
                 'type' => 'input',
                 'value' => null,
@@ -321,7 +290,6 @@ class VariableSeeder extends Seeder
             [
                 //id-32
                 'indicator_id' => 49,
-                'label' => 'ตัวแปร',
                 'variable_name' => 'score',
                 'type' => 'static',
                 'value' => 70.00,
@@ -330,7 +298,6 @@ class VariableSeeder extends Seeder
             [
                 //id-33
                 'indicator_id' => 49,
-                'label' => 'ตัวแปร',
                 'variable_name' => 'score',
                 'type' => 'static',
                 'value' => 25.00,
@@ -339,7 +306,6 @@ class VariableSeeder extends Seeder
             [
                 //id-34
                 'indicator_id' => 49,
-                'label' => 'คะแนนที่ได้',
                 'variable_name' => 'score',
                 'type' => 'output',
                 'value' => null,
@@ -348,7 +314,6 @@ class VariableSeeder extends Seeder
             [
                 //id-35
                 'indicator_id' => 53,
-                'label' => 'ร้อยละของอาจารย์พยาบาลประจำที่ปฏิบัติการพยาบาลย้อนหลัง3ปี',
                 'variable_name' => 'precent_nurse_practice',
                 'type' => 'input',
                 'value' => null,
@@ -357,7 +322,6 @@ class VariableSeeder extends Seeder
             [
                 //id-36
                 'indicator_id' => 55,
-                'label' => 'ร้อยละของบัณฑิตที่มีอัตลักษณ์/คุณลักษณะพิเศษตามที่สถาบันกำหนดต่อจำนวนบัณฑิตทั้งหมด',
                 'variable_name' => 'precent_graduate_identity',
                 'type' => 'input',
                 'value' => null,
