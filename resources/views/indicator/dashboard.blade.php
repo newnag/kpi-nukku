c042e72673be9ac23d1e790779d83509d948d6e0
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-09-02 00:03:49 +0700

    fix: Replace button with anchor tag for indicator detail navigation

--- resources/views/indicator/dashboard.blade.php
+++ resources/views/indicator/dashboard.blade.php
@@ -368,7 +368,7 @@
                             </span>
                         </td>
                         <td class="px-4 py-3 text-sm">
-                            <button {{-- onclick="window.location.href='{{ route('indicator.show', $indicator['id']) }}'" --}}
+                            <a href="{{ route('indicator.show', $indicator['id']) }}"
                                 class="inline-flex items-center justify-center px-3 py-1 bg-white border border-blue-500 text-blue-500 rounded-full text-xs font-medium hover:bg-blue-500 hover:text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 mr-1" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor">
@@ -378,7 +378,7 @@
                                         d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                 </svg>
                                 ดูเพิ่มเติม
-                            </button>
+                            </a>
                         </td>
                     </tr>
                     @empty
