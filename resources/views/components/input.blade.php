27ad368947074f1ad805bf87a5383f39503b7055
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-09-01 11:16:27 +0700

    style: Refactor input component markup for improved readability and consistency

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,17 +2,19 @@
 
 <label class="block">
     @if ($label)
-        <span class="text-sm font-medium text-slate-700">{{ $label }} 
-          @if ($required)
-            <span class="text-red-500">*</span>
-          @endif
+        <span class="text-sm font-medium text-slate-700">{{ $label }}
+            @if ($required)
+                <span class="text-red-500">*</span>
+            @endif
         </span>
     @endif
     <input type="{{ $type }}" name="{{ $name }}" value="{{ old($name, $value) }}"
-    placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
-    {{ $attributes->merge(['class' =>
-        'p-2 mt-1 w-full bg-white rounded-xl border border-slate-300 
-        placeholder-slate-400 text-sm md:text-base 
-        hover:shadow-md hover:border-blue-400 transition
-        ']) }}>
+        placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
+        {{ $attributes->merge([
+            'class' => 'p-2 mt-1 w-full bg-white rounded-xl border border-slate-300 
+                placeholder-slate-400 text-sm md:text-base 
+                hover:shadow-md hover:border-blue-400 transition
+                focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500
+                ',
+        ]) }}>
 </label>
