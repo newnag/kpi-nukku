1f2505bd2363c2c55444e613a08472936aa798f4
พิสุทธิ์ พาละพล <55123204+Pisutpa@users.noreply.github.com>
2025-10-11 17:43:20 +0700

    feat: Add components CSS file to Vite configuration input

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css', 
                 'resources/css/layout.css',
-                'resources/js/app.js'
+                'resources/js/app.js',
+                'resources/css/components.css'
             ],
             refresh: true,
         }),
